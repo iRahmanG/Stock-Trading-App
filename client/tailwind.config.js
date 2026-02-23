76670241eba1ffa707f1d9ce5b37225e426d13c6
iRahmanG <rahmanmr039@gmail.com>
2026-02-24 00:19:22 +0530

    chore: configure Tailwind CSS, React Router, and folder structure for frontend

---
+++ client/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        "primary": "#1152d4",
+        "background-light": "#f6f6f8",
+        "background-dark": "#101622",
+      },
+      fontFamily: {
+        "display": ["Manrope", "sans-serif"],
+        "body": ["Manrope", "sans-serif"]
+      },
+    },
+  },
+  plugins: [],
+}
